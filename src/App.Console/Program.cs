3520c32059ad38dcf828e207b61f0906f779770e
TheLonelyCoder <thelonelycoder@inbox.com>
2026-02-11 10:08:15 +0100

    Test carrier improved

--- src/App.Console/Program.cs
+++ src/App.Console/Program.cs
@@ -10,6 +10,7 @@
     --------------------------------------------------------------------------------------------------------------------------
     DATE          VERSION     DESCRITPION
     --------------------------------------------------------------------------------------------------------------------------
+    2026-02-11    0.0.1.7     Code structure 
     2026-02-08    0.0.0.6     "Ctrl-C" Cleanup activated
     2026-02-08    0.0.0.5     Cursor ON / OFF testing
     2026-02-07    0.0.0.4     Primary/Secondary Screen Buffer tested
@@ -23,25 +24,31 @@
 
 using AnsiVideoTerminal;
 
-var appVer = typeof(Program).Assembly.GetName().Version?.ToString() ?? "?";
-var libVer = typeof(VideoTerminal).Assembly.GetName().Version?.ToString() ?? "?";
+var _appVer = typeof(Program).Assembly.GetName().Version?.ToString() ?? "?";
+var _libVer = typeof(VideoTerminal).Assembly.GetName().Version?.ToString() ?? "?";
 
 AnsiSetup.SetupAnsiTerminal();
 
-var vt = new VideoTerminal();
-AppDomain.CurrentDomain.ProcessExit += (_, __) => vt.Dispose();
-Console.CancelKeyPress += (_, __) => vt.Dispose();
-        
-vt.CursorOff();
-vt.UseSecondaryBuffer();
-vt.ClearScreen();
-vt.SetColor(TerminalColors.Green, TerminalColors.Black);
-vt.Box();
-vt.Write(2, 10, "Programm Version " + appVer);
-vt.Write(3, 10, "VideoTerminal Version " + libVer);
-vt.Write( 5, 5, "ANSI ready");
-vt.Write( 9, 5, "Press any key...");
-Console.ReadKey(true);
-vt.UsePrimaryBuffer();
-vt.CursorOn();
+using (var _vt = new VideoTerminal())
+{
+    AppDomain.CurrentDomain.ProcessExit += (_, __) => _vt.Dispose();
+    Console.CancelKeyPress += (_, __) => _vt.Dispose();
+    RunBasicTest(_vt, _appVer, _libVer);
+}
 
+
+void RunBasicTest(VideoTerminal vt, string appVer, string libVer)
+{
+    vt.CursorOff();
+    vt.UseSecondaryBuffer();
+    vt.ClearScreen();
+    vt.SetColor(TerminalColors.Green, TerminalColors.Black);
+    vt.Box();
+    vt.Write( 2, 10, "Programm Version " + appVer);
+    vt.Write( 3, 10, "VideoTerminal Version " + libVer);
+    vt.Write( 5,  5, "ANSI Terminal ready for more code!");
+    vt.Write( 9,  5, "Press any key...");
+    Console.ReadKey(true);
+    vt.UsePrimaryBuffer();
+    vt.CursorOn();
+}
